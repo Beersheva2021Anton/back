afe1ef7fbba3a59fcad9b57bb828cd53f26d29dd
Anton Elin <checkano@gmail.com>
2022-02-24 13:03:34 +0200

    Add stop words to config

--- spring-intro/src/main/java/telran/spring/aop/ValidationAspect.java
+++ spring-intro/src/main/java/telran/spring/aop/ValidationAspect.java
@@ -3,16 +3,21 @@ package telran.spring.aop;
 import java.util.*;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class ValidationAspect {
 	
-	private HashSet<String> deniedWords = new HashSet<>(Arrays.asList("C#", "C++", "Phyton"));
+	@Value("${aop.telran.stopwords}")
+	private String[] stopWords;
 	
 	@Around("execution(public * telran.spring.service..send(..))")
 	Object validator(ProceedingJoinPoint jp) throws Throwable {
+		
+		var deniedWords = new HashSet<>(Arrays.asList(stopWords));
+		
 		var args = jp.getArgs();
 		var message = args[0];
 		var words = message.toString().split(" ");
